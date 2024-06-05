93e6403a2e59c53e949525525df1578fb5b18d87
JJamC <jaycarroll97@live.co.uk>
2024-06-05 10:24:48 +0100

    added loading to user page

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { fetchUser } from "../api";
 import { useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 export default function Users() {
 
-    const [user, setUser] = useState({})
+  const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchUser().then((userData) => {
@@ -14,20 +17,28 @@ export default function Users() {
         })
     }, [])
 
-    return (
+    return loading ? (
+      <Box sx={{ display: "flex", justifyContent: "center", margin: "20px" }}>
+        <CircularProgress />
+      </Box>
+    ) : (
       <div className="users-page">
         <h2 className="user-header">Users</h2>
         <div className="user-card">
           <div className="user-info">
-          <h3 className="user-card-header">{user.username}'s Profile</h3>
-            <p>Username: <br/>
-              {user.username}</p>
-            <p>Full Name: <br/>
-              {user.name}</p>
-            </div>
+            <h3 className="user-card-header">{user.username}'s Profile</h3>
+            <p>
+              Username: <br />
+              {user.username}
+            </p>
+            <p>
+              Full Name: <br />
+              {user.name}
+            </p>
+          </div>
           <div className="user-profile-pic">
             <img src={user.avatar_url} />
-            </div>
+          </div>
         </div>
       </div>
     );
